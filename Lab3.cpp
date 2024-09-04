2a2a07b2374d4cc58d84a8c7168c55bb86e36f24
Zookes <90047175+Zookes@users.noreply.github.com>
2024-09-04 13:43:36 -0400

    Update Lab3.cpp
    Should be the completed code.

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,23 +1,46 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
+
 using namespace std;
+
 void printMenu(int& choice) {
 	cout << "Welcome to my program, please select an operation to perform:" << endl;
-	cout << "1. Placeholder" << endl;
-	cout << "2. Another Placeholder" << endl;
-	cout << "3. And a third one" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
 	cout << "\nYour Selection: ";
 	cin >> choice;
 }
+
 void getChoices(float& A, float& B) {
-	cout << "Please enter the first value:";
+	cout << "Please enter the first value: ";
 	cin >> A;
+	cout << "Please enter the second value: ";
+	cin >> B;
 	// The rest of this function is an exercise to the reader
 }
 void firstChoice(float A, float B) {
-	cout << "This is proving the first choice ran" << endl;
+    float X;
+    X = A + B;
+	cout << A << " + " << B  << " = " << X << endl;
 }
 void secondChoice(float A, float B) {
-	cout << "This is proving the second choice ran" << endl;
+	float X;
+	X = A - B;
+	cout << A << " - " << B  << " = " << X << endl;
+}
+
+void thirdChoice(float A, float B) {
+    float X;
+    X = A * B;
+	cout << A << " * " << B  << " = " << X << endl;
+}
+
+void fourthChoice(float A, float B) {
+    float X;
+    X = A / B;
+	cout << A << " / " << B  << " = " << X << endl;
 }
 int main() {
 	int choice;
@@ -30,6 +53,14 @@ int main() {
 	}
 	if (choice == 2) {
 		secondChoice(A, B);
+	}
+	
+	if (choice == 3){
+	    thirdChoice(A, B);
+	}
+	
+	if (choice == 4){
+	    fourthChoice(A, B);
 	}
 	return 0;
 }
